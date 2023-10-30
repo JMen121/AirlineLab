847f3a64f74bbaf8828a8bb52100b2fb1e08bae6
Janet_Mensah <janetmensah60@gmail.com>
2023-10-30 11:35:52 +0000

    Begin the intial menu and methods for the command line

--- AirlineLab/src/main/java/Airline/Main.java
+++ AirlineLab/src/main/java/Airline/Main.java
@@ -1,20 +1,17 @@
 package Airline;
-
-import Airline.Flight;
-import Airline.Passenger;
-import Airline.ManagingSystem;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.StrictMath.random;
+
 public class Main {
-   private ArrayList<Flight>flights = new ArrayList<>();
+   //private ArrayList<Flight>flights = new ArrayList<>();
     private ArrayList<Passenger> passengers = new ArrayList<>();
-    private ArrayList<Flight> flightList = new ArrayList<>();
+    private static ArrayList<Flight> flightList = new ArrayList<>();
    private  ArrayList<Passenger> bookedList = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Welcome to BAFA Airline, the no.1 Airline for comfort!");
+        System.out.println("Welcome to Bright Airline App, the no.1 Airline for comfort!");
         System.out.println("What is your name?");
 
         Scanner sc = new Scanner(System.in);
@@ -22,40 +19,61 @@ public class Main {
         System.out.println("Welcome " + username);
 
         System.out.println("Where would you like to explore today " + username + "?");
+        System.out.println("Select a number to get started");
 
-
-        System.out.println(" 1. Add Your Existing FLight");
-        System.out.println(" 2. ");
-        System.out.println(" 3. Book a Flight");
-        System.out.println(" 4. Book a Flight");
-        System.out.println(" 5. Book a Flight");
+           //I want users to be able to write the number and then be directed to the page
+        System.out.println(" 1. Add a Flight");
+        System.out.println(" 2. Check Flight Status");
+        System.out.println(" 3. Display all Flights");
+        System.out.println(" 4. Cancel a Flight");
+        System.out.println(" 5. Create a passenger");
+        System.out.println(" 6. Remove Passenger");
+        System.out.println(" 7. View Passengers");
+        System.out.println(" 8. Add Passenger to a Flight");
         System.out.println("Menu");
 
+        int input = sc.nextInt();
+        switch (input){
+            case 1:
+                addFlight();
+                break;
+
+            case 2 :
+               // checkStatus();
+                break;
+
+            case 3 :
+               // displayAllFlights();
+                break;
+
+            case 4:
+               // cancelFlight();
+
+        }
+
+    }
+
+    private static void addFlight() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Destination");
+        String destination = sc.nextLine();
+        int flightID= random();
+        Flight flight = new Flight(destination,flightID);
+        System.out.println();
 
 
+
+
+
+
+        // when user inputs the flight number make sure it is added to the list of array for flights
 
 
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
 
 
 
